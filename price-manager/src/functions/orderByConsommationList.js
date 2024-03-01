4a92c77c4aea93e07bdbbdf84eccbeae1ee47402
unknown <emotumdev@gmail.com>
2024-03-01 12:41:34 +0100

    filtre by site date consommation price and difference OK with reverse option for each filters

--- price-manager/src/functions/orderByConsommationList.js
+++ price-manager/src/functions/orderByConsommationList.js
@@ -1,57 +1,10 @@
 import React from "react";
-
-function quicksortConsommation(fileContent)
-{
-    var items = fileContent;
-    function swap(items, leftIndex, rightIndex){
-        var temp = items[leftIndex];
-        items[leftIndex] = items[rightIndex];
-        items[rightIndex] = temp;
-    }
-
-    function partition(items, left, right) {
-        var pivot = items[Math.floor((right + left) / 2)].consommation, //middle element
-            i = left, //left pointer
-            j = right; //right pointer
-        while (i <= j) {
-            while (items[i].consommation < pivot) {
-                i++;
-            }
-            while (items[j].consommation > pivot) {
-                j--;
-            }
-            if (i <= j) {
-                swap(items, i, j); //sawpping two elements
-                i++;
-                j--;
-            }
-        }
-        return i;
-    }
-
-    function quickSort(items, left, right) {
-        var index;
-        if (items.length > 1) {
-            index = partition(items, left, right); //index returned from partition
-            if (left < index - 1) { //more elements on the left side of the pivot
-                quickSort(items, left, index - 1);
-            }
-            if (index < right) { //more elements on the right side of the pivot
-                quickSort(items, index, right);
-            }
-        }
-        return items;
-    }
-    // first call to quick sort
-    var sortedArray = quickSort(items, 0, items.length - 1);
-    return sortedArray;
-
-}
+import quicksort from "./quicksort";
 
 
 function orderByConsommationList(fileContent) {
 
-    return quicksortConsommation(fileContent);
+    return quicksort(fileContent, "consommation");
 }
 
 export default orderByConsommationList;
